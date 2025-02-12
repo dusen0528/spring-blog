62abc962850a3cd5298e7f157301c269d3591c8e
inho <dusen0528@naver.com>
2025-02-12 10:49:08 +0900

    테스트코드 추가

---
+++ src/test/java/com/nhnacademy/blog/hello/TestClass.java
@@ -0,0 +1,12 @@
+package com.nhnacademy.blog.hello;
+
+public class TestClass {
+    private String name;
+
+    public TestClass() {
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/test/java/com/nhnacademy/blog/hello/stereotype/repository/HelloRepository.java
@@ -0,0 +1,4 @@
+package com.nhnacademy.blog.hello.stereotype.repository;
+
+public interface HelloRepository {
+}
